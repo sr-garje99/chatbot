644a40c471d4956164e5bbd6bfd9e2305faeed38
sr-garje99 <shubham.garje198@gmail.com>
2024-01-08 23:14:47 +0530

    chat window and button

---
+++ src/copmponents/ChatWindow.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ChatWindow = () => {
+  return (
+    <div className='Card' style={{width:"30rem",hright:"100rem",backgroundColor:"skyblue",border:"1rem",
+       borderBlockColor:"white" }}>
+          <div>
+                Header
+        </div>
+        ChatWindow
+      
+        <div>
+            Input
+        </div>
+    </div>
+
+  )
+}
+
+export default ChatWindow
