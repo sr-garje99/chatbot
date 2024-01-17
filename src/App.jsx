1aa163b8e36043dd71fafd384d32c9793df640a5
sr-garje99 <shubham.garje198@gmail.com>
2024-01-17 23:08:58 +0530

    imported chat-bot-kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import { StyledEngineProvider,CssBaseline, } from '@mui/material'
 //import './App.css'
+
+
+
 import ChatWindow from './copmponents/ChatWindow'
 import {Button} from '@mui/material'
+//import ChatBot from './copmponents/ChatBot'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +15,12 @@ function App() {
     <>
         <StyledEngineProvider injectFirst>
           <CssBaseline/>
+          <div style={{display:"flex",justifyContent:"right",float:"right"}}>
            {count && <ChatWindow/>}
 
            <Button style={{float:"left"}}onClick={()=>{setCount(!count)}}>chat</Button>
+           {/* <ChatBot/> */}
+           </div>
         </StyledEngineProvider>
     </>
   )
